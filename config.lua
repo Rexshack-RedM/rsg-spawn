cd883a01feca7b18275b63fc78e6b13685b4f3c3
RexShack <rexshackuk@gmail.com>
2025-11-10 04:35:23 +0000

    correct spelling

--- config.lua
+++ config.lua
@@ -7,7 +7,7 @@ Config.AutoDualWield = true
 Config.SpawnLocation = { coords = vector4(-169.47, 629.38, 114.03, 236.72) } -- valentine station
 
 Config.RandomTips = {
-    'TIP : use [LALT] to targert',
+    'TIP : use [LALT] to target',
     'TIP : use [H] to call your horse',
     'TIP : use [I] to open your inventory',
 }
